c0b5b7945743276435392ef34f8584ccc11a317a
Abrham-Yifter <ayifter@excellerentsolutions.com>
2021-08-18 14:14:08 +0300

    "Department Entity and Model Initially Added"

---
+++ Studmgt.Domain/Model/Department.cs
@@ -0,0 +1,11 @@
+﻿
+using Studmgt.Domain.Seeds;
+
+namespace Studmgt.Domain.Model
+{
+    public class Department: BaseAuditableModels
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
