ec5435e54939ecd0129fa06478d633cd311a9f23
fikremariam <fikrenew@gmail.com>
2021-08-18 14:23:56 +0300

    Course model and entity added

---
+++ Studmgt.Domain/Entity/CourseEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Studmgt.Domain.Entity
+{
+  public  class CourseEntity
+    {
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public string Location { get; set; }
+        public string Description { get; set; }
+    }
+}
