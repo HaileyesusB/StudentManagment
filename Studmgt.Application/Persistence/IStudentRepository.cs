a4ef0e0cdc3744750b419aa0e73fa76cd6cdd57a
Haileyesus Belew <HBelew@excellerentsolutions.com>
2021-08-19 23:14:11 -0700

    Service Student removed

---
+++ Studmgt.Application/Persistence/IStudentRepository.cs
@@ -0,0 +1,13 @@
+﻿using Studmgt.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Studmgt.Application.Persistence
+{
+    public interface IStudentRepository : IAsyncRepository<Student>
+    {
+        Task<IEnumerable<Student>> GetStudentById(string id);
+    }
+}
---
+++ Studmgt.Application/Persistence/SortStudent.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Studmgt.Application.Persistence
+{
+  public enum SortStudent
+    {
+        Ascending = 0,
+        Descending = 1
+    }
+}
